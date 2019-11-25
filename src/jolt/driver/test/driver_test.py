54f996a55c243a1e5e251622a99b7775ae75c6e7
Philip Winkler <winkler@delmic.com>
2019-11-25 10:51:29 +0100

    Added GPL2 licence

--- src/jolt/driver/test/driver_test.py
+++ src/jolt/driver/test/driver_test.py
@@ -5,7 +5,17 @@ Created on 1 Oct 2019
 
 Copyright © 2017-2018 Anders Muskens, Philip Winkler, Delmic
 
-Licence
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see http://www.gnu.org/licenses/.
 '''
 
 

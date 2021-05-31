c6416e2260aacfd162966af99b69cd0aae486dd5
VictoriaDel <mavrikopoulou@delmic.com>
2021-05-31 18:19:01 +0200

    Update the python path in the bat files

--- install/windows/build_jolt.bat
+++ install/windows/build_jolt.bat
@@ -1,5 +1,5 @@
 setlocal
 SET "scriptdir=%~dp0"
-SET PYTHONPATH=%scriptdir%\..\src\;%PYTHONPATH%
-python C:\development\jolt-control\install\windows\build.py
+SET PYTHONPATH=%scriptdir%\..\..\src\;%PYTHONPATH%
+python %scriptdir%\build.py
 endlocal
--- install/windows/run_jolt_from_source.bat
+++ install/windows/run_jolt_from_source.bat
@@ -1,7 +1,7 @@
 setlocal
 SET "scriptdir=%~dp0"
-SET PYTHONPATH=%scriptdir%\..\src\;%PYTHONPATH%
+SET PYTHONPATH=%scriptdir%\..\..\src\;%PYTHONPATH%
 REM Uncomment to run with the simulator
 REM set TEST_NOHW=1
-python %scriptdir%\..\src\jolt\gui\jolt_app.py
+python %scriptdir%\..\..\src\jolt\gui\jolt_app.py
 endlocal

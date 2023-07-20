e854ef013390cfb36af16cb12b9ac7a30801b348
Éric Piel <piel@delmic.com>
2023-07-20 18:09:01 +0800

    prepare for v1.2.0

--- src/jolt/__init__.py
+++ src/jolt/__init__.py
@@ -17,8 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, see http://www.gnu.org/licenses/.
 '''
 
-__version__ = "1.1.1-dev"
+__version__ = "1.2.0"
 __fullname__ = "Delmic Jolt Control Software"
 __shortname__ = "Jolt"
-__copyright__ = "Copyright © 2019-2022 Delmic"
+__copyright__ = "Copyright © 2019-2023 Delmic"
 __authors__ = ["Éric Piel", "Philip Winkler", "Anders Muskens", "Victoria Mavrikopoulou"]
